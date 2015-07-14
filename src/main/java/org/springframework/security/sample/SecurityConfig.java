9677e6c99ffd492b8548f700d4e2bf7704d88e22
Rob Winch <rwinch@gopivotal.com>
2015-07-14 10:17:13 -0500

    override RequestDataValueProcessor

--- src/main/java/org/springframework/security/sample/SecurityConfig.java
+++ src/main/java/org/springframework/security/sample/SecurityConfig.java
@@ -1,8 +1,15 @@
 package org.springframework.security.sample;
 
+import java.util.Collections;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.support.RequestDataValueProcessor;
 
 @EnableWebSecurity
 public class SecurityConfig {
@@ -12,5 +19,33 @@ public class SecurityConfig {
 		auth
 			.inMemoryAuthentication()
 				.withUser("user").password("password").roles("USER");
+	}
+
+	@Bean
+	public RequestDataValueProcessor requestDataValueProcessor() {
+		return new RequestDataValueProcessor() {
+
+			public String processAction(HttpServletRequest request, String action) {
+				return action+"?processed";
+			}
+
+			public String processAction(HttpServletRequest request, String action, String method) {
+
+				return action+"?processed";
+			}
+
+			public String processFormFieldValue(HttpServletRequest request, String name,
+					String value, String type) {
+				return value;
+			}
+
+			public Map<String, String> getExtraHiddenFields(HttpServletRequest request) {
+				return Collections.emptyMap();
+			}
+
+			public String processUrl(HttpServletRequest request, String url) {
+				return url;
+			}
+		};
 	}
 }
