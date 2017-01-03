1ddfce68bf5ee8e5bf83ff847792baa87a9514b9
Stefan Pfeiffer <pfeiffer.stefan@inside-online.de>
2017-01-03 15:12:18 +0100

    Modify Example to trigger failing test

--- src/main/java/com/example/SecurityConfig.java
+++ src/main/java/com/example/SecurityConfig.java
@@ -1,9 +1,26 @@
 package com.example;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    super.configure(http);
+    LoginUrlAuthenticationEntryPoint EP = new LoginUrlAuthenticationEntryPoint("/login") {
+      @Override
+      protected String buildRedirectUrlToLoginPage(HttpServletRequest request,
+			HttpServletResponse response, AuthenticationException authException) {
+        return this.getLoginFormUrl();
+      };
+      
+    };
+    http.exceptionHandling().authenticationEntryPoint(EP);
+  }
 }
