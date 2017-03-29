8d2ddbbe6060a277c8d8cfa4ec5a43cceacd77b2
Rob Winch <rwinch@users.noreply.github.com>
2017-03-29 14:24:17 -0500

    Saving

--- src/test/java/demo/ApplicationTests.java
+++ src/test/java/demo/ApplicationTests.java
@@ -1,47 +1,29 @@
 package demo;
 
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
+
+import com.gargoylesoftware.htmlunit.WebClient;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+@WebMvcTest
 public class ApplicationTests {
 	@Autowired
-	WebApplicationContext context;
-
-	MockMvc mockMvc;
-
-	@Before
-	public void setup() {
-		mockMvc = MockMvcBuilders
-				.webAppContextSetup(context)
-				.apply(springSecurity())
-				.build();
-	}
-
-	@Test
-	public void requiresLogin() throws Exception {
-		mockMvc.perform(get("/"))
-			.andExpect(status().is3xxRedirection());
-	}
+	WebClient webClient;
 
 	@WithMockUser
 	@Test
-	public void authenticatedWorks() throws Exception {
-		mockMvc.perform(get("/"))
-			.andExpect(status().is2xxSuccessful());
+	public void authenticated() throws Exception {
+		webClient.getPage("/");
+	}
+
+
+	@Test
+	public void notAuthenticated() throws Exception {
+		webClient.getPage("/");
 	}
 }
