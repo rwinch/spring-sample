89f8b0e7b14abf8c136c0558fc3492c866379293
Rob Winch <rwinch@users.noreply.github.com>
2017-11-03 12:22:25 -0500

    Simplify WebSecurityConfig

--- src/main/java/demo/WebSecurityConfig.java
+++ src/main/java/demo/WebSecurityConfig.java
@@ -15,23 +15,31 @@
  */
 package demo;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  * @author Rob Winch
  *
  */
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+@Configuration
+public class WebSecurityConfig {
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.httpBasic().and()
-			.cors().and()
-			.authorizeRequests()
-				.anyRequest().authenticated();
+	@Bean
+	public static InMemoryUserDetailsManager userDetailsManager() {
+		UserDetails user = User.withDefaultPasswordEncoder()
+			.username("user")
+				.password("password")
+				.roles("USER")
+				.build();
+		return new InMemoryUserDetailsManager(user);
 	}
 }
