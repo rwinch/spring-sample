222c185f694e323100c8d0bd9532c3ea0240d983
Rob Winch <rwinch@users.noreply.github.com>
2018-08-21 12:22:43 -0500

    Demo WebFlux

--- src/test/java/demo/ApplicationTests.java
+++ src/test/java/demo/ApplicationTests.java
@@ -3,12 +3,17 @@ package demo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Base64;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -17,35 +22,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
-@AutoConfigureMockMvc
+@AutoConfigureWebTestClient
 public class ApplicationTests {
 	@Autowired
-	MockMvc mockMvc;
-
-	@Test
-	public void requiresLogin() throws Exception {
-		this.mockMvc.perform(get("/").accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isUnauthorized());
-	}
-
-	@Test
-	public void httpBasicWorks() throws Exception {
-		this.mockMvc.perform(get("/").with(httpBasic("user","password")))
-				.andExpect(status().isOk())
-				.andExpect(content().string("Hello Boot!"));
-	}
+	WebTestClient rest;
 
 	@Test
 	public void badCredentials() throws Exception {
-		this.mockMvc.perform(get("/").with(httpBasic("invalid", "creds")))
-				.andExpect(status().isUnauthorized());
-	}
-
-	@WithMockUser
-	@Test
-	public void authenticatedWorks() throws Exception {
-		this.mockMvc.perform(get("/"))
-			.andExpect(status().is2xxSuccessful())
-			.andExpect(content().string("Hello Boot!"));
+		this.rest.get().uri("/")
+				.header(HttpHeaders.AUTHORIZATION, Base64.getEncoder().encodeToString("invalid:creds".getBytes()))
+				.exchange()
+				.expectStatus().isUnauthorized();
 	}
 }
