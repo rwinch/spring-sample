fe247fae6d92e215f15f765932ec524022b832b9
Rob Winch <rwinch@users.noreply.github.com>
2018-11-05 16:00:06 -0600

    Boot 2.0

--- src/main/java/demo/MessageController.java
+++ src/main/java/demo/MessageController.java
@@ -15,14 +15,10 @@
  */
 package demo;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin
 public class MessageController {
 
 	@GetMapping("/")
