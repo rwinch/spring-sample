27548f30154b517028b30894043b6729cfe67cd7
Rob Winch <rwinch@users.noreply.github.com>
2020-03-03 15:54:51 -0600

    Demo gh-8053

--- src/test/java/demo/ApplicationTests.java
+++ src/test/java/demo/ApplicationTests.java
@@ -5,17 +5,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class ApplicationTests {
@@ -23,23 +19,7 @@ public class ApplicationTests {
 	MockMvc mockMvc;
 
 	@Test
-	public void requiresLogin() throws Exception {
-		this.mockMvc.perform(get("/").accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isUnauthorized());
-	}
-
-	@Test
-	public void httpBasicWorks() throws Exception {
-		this.mockMvc.perform(get("/").with(httpBasic("user","password")))
-				.andExpect(status().isOk())
-				.andExpect(content().string("Hello Boot!"));
-	}
-
-	@WithMockUser
-	@Test
-	public void authenticatedWorks() throws Exception {
-		this.mockMvc.perform(get("/"))
-			.andExpect(status().is2xxSuccessful())
-			.andExpect(content().string("Hello Boot!"));
+	public void oauthAuthorizeThenRedirectsToMclogin() throws Exception {
+		this.mockMvc.perform(get("/oauth/authorize")).andExpect(redirectedUrl("http://localhost/mclogin"));
 	}
 }
