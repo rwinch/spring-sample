7cc6598b745e0579afe7caace116f1d96f1ca68f
Rob Winch <rwinch@users.noreply.github.com>
2020-04-28 12:29:52 -0500

    Cleanup Imports

--- src/main/java/demo/WebSecurityConfig.java
+++ src/main/java/demo/WebSecurityConfig.java
@@ -18,12 +18,9 @@ package demo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
