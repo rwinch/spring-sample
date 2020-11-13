66a8192de569915ffe313c9f7be7107d3cb640fe
Rob Winch <rwinch@users.noreply.github.com>
2020-11-13 13:32:10 -0600

    Demo

--- src/main/java/demo/Application.java
+++ src/main/java/demo/Application.java
@@ -2,8 +2,9 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityFilterAutoConfiguration.class)
 public class Application {
 
 	public static void main(String[] args) {
