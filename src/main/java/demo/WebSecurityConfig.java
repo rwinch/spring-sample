575769778eac76dfac6d5f8956930669a771a38f
Rob Winch <rwinch@users.noreply.github.com>
2021-03-30 17:30:06 -0500

    Saving

--- src/main/java/demo/WebSecurityConfig.java
+++ src/main/java/demo/WebSecurityConfig.java
@@ -1,36 +1,38 @@
 package demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+public class WebSecurityConfig {
+	@Bean
+	public DefaultSecurityFilterChain configure(AuthenticationFilter authenticationFilter, HttpSecurity http) throws Exception {
+		if (authenticationFilter != null) {
+			http.addFilter(authenticationFilter);
+		}
 		http
 			.authorizeRequests()
 				.anyRequest().authenticated()
 				.and()
 			.httpBasic();
+		return http.build();
 	}
 
-	private String pathVariable(String pattern, String name) {
-		return "@pathVariableEvaluator.extract(request,'"+pattern+"')['"+ name +"']";
-	}
-
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth
-			.inMemoryAuthentication()
-				.withUser("user").password("{noop}password").roles("USER").and()
-				.withUser("admin").password("{noop}password").roles("USER");
-
+	@Bean
+	public InMemoryUserDetailsManager userDetails() throws Exception {
+		UserDetails user = User.withDefaultPasswordEncoder()
+			.username("user")
+			.password("password")
+			.build();
+		return new InMemoryUserDetailsManager(user);
 	}
 
 }
