ab533a97e6c9ac31b95db41b43b9b55ee7ba0983
Rob Winch <rwinch@users.noreply.github.com>
2021-03-30 17:41:54 -0500

    Modernize a bit

--- src/main/java/demo/WebSecurityConfig.java
+++ src/main/java/demo/WebSecurityConfig.java
@@ -2,27 +2,24 @@ package demo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.DefaultSecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 	@Bean
-	public DefaultSecurityFilterChain configure(AuthenticationFilter authenticationFilter, HttpSecurity http) throws Exception {
-		if (authenticationFilter != null) {
-			http.addFilter(authenticationFilter);
-		}
+	public DefaultSecurityFilterChain configure(HttpSecurity http) throws Exception {
 		http
-			.authorizeRequests()
+			.authorizeRequests(requests -> requests
 				.anyRequest().authenticated()
-				.and()
-			.httpBasic();
+			)
+			.httpBasic(Customizer.withDefaults());
 		return http.build();
 	}
 
@@ -31,6 +28,7 @@ public class WebSecurityConfig {
 		UserDetails user = User.withDefaultPasswordEncoder()
 			.username("user")
 			.password("password")
+			.roles("USER")
 			.build();
 		return new InMemoryUserDetailsManager(user);
 	}
--- src/test/java/demo/SpringBootSecurityApplicationTests.java
+++ src/test/java/demo/SpringBootSecurityApplicationTests.java
@@ -1,37 +1,25 @@
 package demo;
 
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = SpringBootSecurityApplication.class)
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
 @WebAppConfiguration
+@AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
 public class SpringBootSecurityApplicationTests {
 	@Autowired
-	WebApplicationContext context;
-
 	MockMvc mockMvc;
-
-	@BeforeEach
-	public void setup() {
-		mockMvc = MockMvcBuilders
-				.webAppContextSetup(context)
-				.apply(springSecurity())
-				.build();
-	}
 
 	@Test
 	public void messageWhenNotAuthenticatedThenUnauthorized() throws Exception {
@@ -39,8 +27,8 @@ public class SpringBootSecurityApplicationTests {
 			.andExpect(status().isUnauthorized());
 	}
 
-	@WithMockUser
 	@Test
+	@WithMockUser
 	public void messageWhenUserThenOk() throws Exception {
 		mockMvc.perform(get("/message"))
 			.andExpect(status().isOk());
