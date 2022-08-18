fafb14d1ad78ec14416818789c34b4a076ec3357
Rob Winch <rwinch@users.noreply.github.com>
2022-08-18 10:36:54 -0500

    Add SessionConfiguration

---
+++ src/main/java/example/SessionConfiguration.java
@@ -0,0 +1,30 @@
+package example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
+import org.springframework.session.data.redis.RedisSessionRepository;
+
+@Configuration(proxyBeanMethods = false)
+@EnableSpringHttpSession
+public class SessionConfiguration {
+
+
+
+	@Bean
+	RedisSessionRepository redisSessionRepository(RedisTemplate<String, Object> springSessionRedisTemplate) {
+		return new RedisSessionRepository(springSessionRedisTemplate);
+	}
+
+	@Bean
+	RedisTemplate<String, Object> springSessionRedisTempate(RedisConnectionFactory redisConnectionFactory) {
+		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+		redisTemplate.setConnectionFactory(redisConnectionFactory);
+		return redisTemplate;
+	}
+}
