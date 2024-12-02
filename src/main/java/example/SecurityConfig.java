6bb2dadc3c2814979e9cc8b8560dc14e33cda3b9
Rob Winch <362503+rwinch@users.noreply.github.com>
2024-12-02 13:36:59 -0600

    saving

--- src/main/java/example/SecurityConfig.java
+++ src/main/java/example/SecurityConfig.java
@@ -1,31 +1,71 @@
 package example;
 
+import java.util.Set;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.webauthn.api.AuthenticatorAttachment;
+import org.springframework.security.web.webauthn.api.AuthenticatorSelectionCriteria;
+import org.springframework.security.web.webauthn.api.PublicKeyCredentialRpEntity;
+import org.springframework.security.web.webauthn.api.ResidentKeyRequirement;
+import org.springframework.security.web.webauthn.api.UserVerificationRequirement;
+import org.springframework.security.web.webauthn.management.MapPublicKeyCredentialUserEntityRepository;
+import org.springframework.security.web.webauthn.management.MapUserCredentialRepository;
+import org.springframework.security.web.webauthn.management.PublicKeyCredentialUserEntityRepository;
+import org.springframework.security.web.webauthn.management.UserCredentialRepository;
+import org.springframework.security.web.webauthn.management.WebAuthnRelyingPartyOperations;
+import org.springframework.security.web.webauthn.management.Webauthn4JRelyingPartyOperations;
 
 @Configuration(proxyBeanMethods = false)
 class SecurityConfig {
 	@Bean
-	SecurityWebFilterChain springSecurity(ServerHttpSecurity http) {
+	DefaultSecurityFilterChain springSecurity(HttpSecurity http) throws Exception {
 		http
-			.httpBasic(Customizer.withDefaults())
-			.authorizeExchange(e -> e
-					.pathMatchers("/api/v1/foo").permitAll()
-					.pathMatchers("/api/v1/bar").authenticated()
-					.pathMatchers("/api/v1/baz").hasRole("BAZ")
+			.authorizeHttpRequests(e -> e
+					.anyRequest().authenticated()
+			)
+			.formLogin(Customizer.withDefaults())
+			.webAuthn((webAuthn) -> webAuthn
+				.rpName("Spring Security Relying Party")
+				.rpId("localhost")
+				.allowedOrigins("http://localhost:8080")
 			);
 		return http.build();
 	}
 
 	@Bean
-	ReactiveUserDetailsService userDetailsService() {
+	MapPublicKeyCredentialUserEntityRepository userEntityRepository() {
+		return new MapPublicKeyCredentialUserEntityRepository();
+	}
+
+	@Bean
+	MapUserCredentialRepository userCredentialRepository() {
+		return new MapUserCredentialRepository();
+	}
+
+	@Bean
+	WebAuthnRelyingPartyOperations relyingPartyOperations(PublicKeyCredentialUserEntityRepository userEntities, UserCredentialRepository userCredentials) {
+		Webauthn4JRelyingPartyOperations result = new Webauthn4JRelyingPartyOperations(userEntities, userCredentials,
+				PublicKeyCredentialRpEntity.builder().id("localhost").name("Spring Security Relying Party").build(), Set.of("http://localhost:8080"));
+		result.setCustomizeCreationOptions(c -> c.authenticatorSelection(
+				AuthenticatorSelectionCriteria.builder()
+						.userVerification(UserVerificationRequirement.PREFERRED)
+						.residentKey(ResidentKeyRequirement.REQUIRED)
+						.authenticatorAttachment(AuthenticatorAttachment.PLATFORM)
+						.build()));
+		return result;
+
+	}
+
+	@Bean
+	UserDetailsService userDetailsService() {
 		UserDetails user = User.withDefaultPasswordEncoder()
 				.username("user")
 				.password("password")
@@ -36,6 +76,6 @@ class SecurityConfig {
 				.password("password")
 				.roles("BAZ")
 				.build();
-		return new MapReactiveUserDetailsService(user, baz);
+		return new InMemoryUserDetailsManager(user, baz);
 	}
 }
