d365ee7af5148d73eb7e6d5d113858d15e127075
Rob Winch <362503+rwinch@users.noreply.github.com>
2025-04-04 12:05:18 -0500

    Rest from start.spring.io

--- src/test/java/example/spring/ApplicationTests.java
+++ src/test/java/example/spring/ApplicationTests.java
@@ -1,4 +1,4 @@
-package example;
+package example.spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,4 +9,5 @@ class ApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
 }
