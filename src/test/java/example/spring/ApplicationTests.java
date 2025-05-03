d87aad4ef6c7d2754e644fbb9204a3ba02281f67
Rob Winch <362503+rwinch@users.noreply.github.com>
2025-05-02 22:37:21 -0500

    How to replace WebClient.exchange & preserve Mono<ClientResponse> return type

--- src/test/java/example/spring/ApplicationTests.java
+++ src/test/java/example/spring/ApplicationTests.java
@@ -1,13 +1,51 @@
 package example.spring;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Mono;
 
-@SpringBootTest
+import org.springframework.web.reactive.function.client.WebClient;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class ApplicationTests {
 
+	WebClient client = WebClient.create();
+
 	@Test
-	void contextLoads() {
+	void exchange() {
+		String body = this.client.get()
+			.uri("https://spring.io/.well-known/security.txt")
+			.exchange()
+			.switchIfEmpty(Mono.error(new RuntimeException("response is null")))
+			.flatMap(r -> r.bodyToMono(String.class))
+			.switchIfEmpty(Mono.error(new RuntimeException("body is null")))
+			.block();
+		// succeeds
+		assertThat(body).isNotNull();
+	}
+
+	@Test
+	void exchangeToMonoMonoJust() {
+		String body = this.client.get()
+			.uri("https://spring.io/.well-known/security.txt")
+			.exchangeToMono(Mono::just)
+			.switchIfEmpty(Mono.error(new RuntimeException("response is null")))
+			.flatMap(r -> r.bodyToMono(String.class))
+			.switchIfEmpty(Mono.error(new RuntimeException("body is null")))
+			.block();
+		// fails with body is null
+		assertThat(body).isNotNull();
+	}
+
+	@Test
+	void exchangeToMono() {
+		String body = this.client.get()
+			.uri("https://spring.io/.well-known/security.txt")
+			.exchangeToMono(r -> r.bodyToMono(String.class))
+			.switchIfEmpty(Mono.error(new RuntimeException("body is null")))
+			.block();
+		// succeeds
+		assertThat(body).isNotNull();
 	}
 
 }
