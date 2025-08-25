5aea15c59e01c08f1bbc19f8e7b32961e1b0a149
Rob Winch <362503+rwinch@users.noreply.github.com>
2025-08-25 14:39:24 -0700

    saving

--- src/main/java/example/spring/Application.java
+++ src/main/java/example/spring/Application.java
@@ -2,9 +2,18 @@ package example.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestClient;
 
 @SpringBootApplication
 public class Application {
+	static void test() {
+		RestClient rest = RestClient.create();
+		rest.get()
+			.uri("http://localhost:8080")
+			.attribute("a", "b")
+			.retrieve()
+			.body(String.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
