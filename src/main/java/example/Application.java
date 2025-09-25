4f4e292f7c748f10e28f904cdb7f480f5e67f655
Rob Winch <362503+rwinch@users.noreply.github.com>
2025-09-25 16:21:18 -0500

    Demo Boot 3.5.6

--- src/main/java/example/Application.java
+++ src/main/java/example/Application.java
@@ -2,11 +2,30 @@ package example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer.authorizationServer;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	SecurityFilterChain springSecurity(HttpSecurity http) throws Exception {
+		http
+			.httpBasic(Customizer.withDefaults())
+			.authorizeHttpRequests(requests -> requests
+				.anyRequest().authenticated()
+			)
+			.with(authorizationServer(), authz -> authz
+				.oidc(Customizer.withDefaults())
+			);
+		return http.build();
 	}
 }
--- src/test/java/example/ApplicationTests.java
+++ src/test/java/example/ApplicationTests.java
@@ -1,12 +1,26 @@
 package example;
 
 import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ApplicationTests {
 
+	@Autowired
+	MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	void indexNotAuthorized() throws Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/"))
+			.andExpect(status().isUnauthorized());
 	}
 }
