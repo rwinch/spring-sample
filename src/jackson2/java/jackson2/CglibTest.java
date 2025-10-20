64ea33400ce742e918ecab7806c98032bedee7ff
Rob Winch <362503+rwinch@users.noreply.github.com>
2025-10-20 11:39:20 -0500

    Demo issue

---
+++ src/jackson2/java/jackson2/CglibTest.java
@@ -0,0 +1,47 @@
+package jackson2;
+
+import java.util.Map;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.aop.framework.ProxyFactory;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+public class CglibTest {
+
+	@Test
+	public void cglibPropertiesNotRendered() throws Exception {
+		ObjectMapper mapper = new ObjectMapper();
+		Person target = new Person("first", "last");
+		ProxyFactory factory = new ProxyFactory(target);
+		factory.setOpaque(true);
+
+		Person proxied = (Person) factory.getProxy();
+		String json = mapper.writeValueAsString(proxied);
+		Map<String, Object> properties = mapper.readValue(json, new TypeReference<>() {
+		});
+		assertThat(properties).containsOnlyKeys("firstName", "lastName");
+	}
+
+	static class Person {
+		private final String firstName;
+
+		private final String lastName;
+
+		public Person(String firstName, String lastName) {
+			this.firstName = firstName;
+			this.lastName = lastName;
+		}
+
+		public String getFirstName() {
+			return firstName;
+		}
+
+		public String getLastName() {
+			return lastName;
+		}
+	}
+}
