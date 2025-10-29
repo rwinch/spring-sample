a94f4ed38ce88f138d1a91671e5a4e692ff19e5a
Rob Winch <362503+rwinch@users.noreply.github.com>
2025-10-29 11:54:05 -0500

    Demo

--- src/test/java/example/ApplicationTests.java
+++ src/test/java/example/ApplicationTests.java
@@ -5,12 +5,19 @@ import org.htmlunit.WebClient;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.web.htmlunit.UriBuilderFactoryWebClient;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.htmlunit.MockMvcWebClientBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -46,6 +53,18 @@ class ApplicationTests {
 			String authorize() {
 				return "<html><head><title>Mock IdP</title></head><body>Mock Identity Provider</body></html>";
 			}
+		}
+
+		@Bean
+		// workaround for https://github.com/spring-projects/spring-boot/issues/47857
+		static MockMvcWebClientBuilder webClientBuilder(MockMvc mockMvc, Environment env) {
+			String serverPort = env.getProperty("server.port", "8080");
+			String baseUriPort = env.getProperty("local.server.port", serverPort);
+			UriComponentsBuilder baseUri = UriComponentsBuilder.fromUriString("http://localhost")
+					.port(baseUriPort);
+			DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory(baseUri);
+			UriBuilderFactoryWebClient delegate = new UriBuilderFactoryWebClient(factory);
+			return MockMvcWebClientBuilder.mockMvcSetup(mockMvc).withDelegate(delegate);
 		}
 	}
 }
