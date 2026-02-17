0d58d59d601f45395a7b2561dbc1f6e0dd8a2e1c
Robert Winch <362503+rwinch@users.noreply.github.com>
2026-02-17 15:20:22 -0600

    saving

--- src/main/java/example/Application.java
+++ src/main/java/example/Application.java
@@ -1,10 +1,30 @@
 package example;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
+
+import java.util.function.Supplier;
 
 @SpringBootApplication
 public class Application {
+
+    @Bean
+    void security(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(requests -> requests
+                .anyRequest().access((AuthorizationManager<RequestAuthorizationContext>) (authentication, context) -> {
+                    HttpServletRequest request = context.getRequest();
+                    return null;
+                })
+            );
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
