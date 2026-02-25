cfc3dba9d1579293871c6806bde07233f1d7794d
Robert Winch <362503+rwinch@users.noreply.github.com>
2026-02-25 09:26:46 -0600

    use SecurityFilterChain Bean

--- src/main/java/example/Application.java
+++ src/main/java/example/Application.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
 
 import java.util.function.Supplier;
@@ -16,14 +18,13 @@ import java.util.function.Supplier;
 public class Application {
 
     @Bean
-    void security(HttpSecurity http) throws Exception {
+    SecurityFilterChain springSecurity(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(requests -> requests
-                .anyRequest().access((AuthorizationManager<RequestAuthorizationContext>) (authentication, context) -> {
-                    HttpServletRequest request = context.getRequest();
-                    return null;
-                })
-            );
+                .anyRequest().authenticated()
+            )
+            .formLogin(Customizer.withDefaults());
+        return http.build();
     }
 
 	public static void main(String[] args) {
