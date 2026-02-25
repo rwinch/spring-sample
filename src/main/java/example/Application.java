381968d6e33a70389f04847281d9073d323869b8
Robert Winch <362503+rwinch@users.noreply.github.com>
2026-02-25 08:33:47 -0600

    replicate issue

--- src/main/java/example/Application.java
+++ src/main/java/example/Application.java
@@ -1,28 +1,19 @@
 package example;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.authorization.AuthorizationDecision;
-import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
-
-import java.util.function.Supplier;
 
 @SpringBootApplication
 public class Application {
 
     @Bean
-    void security(HttpSecurity http) throws Exception {
-        http
+    Customizer<HttpSecurity> security() throws Exception {
+        return http -> http
             .authorizeHttpRequests(requests -> requests
-                .anyRequest().access((AuthorizationManager<RequestAuthorizationContext>) (authentication, context) -> {
-                    HttpServletRequest request = context.getRequest();
-                    return null;
-                })
+                    .requestMatchers("/**").permitAll()
             );
     }
 
--- src/test/java/example/ApplicationTests.java
+++ src/test/java/example/ApplicationTests.java
@@ -1,12 +1,22 @@
 package example;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.web.servlet.client.RestTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureRestTestClient
 class ApplicationTests {
 
+
 	@Test
-	void contextLoads() {
+	void cacheControlSet(@Autowired RestTestClient restClient) {
+		restClient.get()
+			.uri("/").exchangeSuccessfully()
+			.expectHeader().valueEquals(HttpHeaders.CACHE_CONTROL, "no-cache, no-store, max-age=0, must-revalidate");
 	}
 }
