e4d42737231354a72a998546a3933c372ab4e4ca
Robert Winch <362503+rwinch@users.noreply.github.com>
2026-03-13 16:40:31 -0500

    Add HTTP Headers

--- src/main/java/example/SecurityDsl.java
+++ src/main/java/example/SecurityDsl.java
@@ -10,14 +10,25 @@ import org.springframework.beans.factory.BeanRegistry;
 import org.springframework.core.env.Environment;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.FilterChainProxy;
+import org.springframework.security.web.header.HeaderWriterFilter;
+import org.springframework.security.web.header.writers.CacheControlHeadersWriter;
+import org.springframework.security.web.header.writers.HstsHeaderWriter;
+import org.springframework.security.web.header.writers.XContentTypeOptionsHeaderWriter;
+import org.springframework.security.web.header.writers.XXssProtectionHeaderWriter;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.util.matcher.AnyRequestMatcher;
-import org.springframework.stereotype.Component;
 
 public class SecurityDsl implements BeanRegistrar {
 
     public void register(BeanRegistry registry, Environment env) {
         List<Filter> securityFilters = new ArrayList<>();
-
+        securityFilters.add(new HeaderWriterFilter(List.of(
+            new XContentTypeOptionsHeaderWriter(),
+            new XXssProtectionHeaderWriter(),
+            new CacheControlHeadersWriter(),
+            new HstsHeaderWriter(),
+            new XFrameOptionsHeaderWriter()
+        )));
 
         FilterChainProxy springSecurityFilterChain = new FilterChainProxy(new DefaultSecurityFilterChain(AnyRequestMatcher.INSTANCE, securityFilters));
         registry.registerBean("springSecurityFilterChain", FilterChainProxy.class, spec ->
--- src/test/java/example/ApplicationTests.java
+++ src/test/java/example/ApplicationTests.java
@@ -3,16 +3,25 @@ package example;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.context.ApplicationContext;
 import org.springframework.security.web.FilterChainProxy;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ApplicationTests {
 
 	@Autowired
 	private ApplicationContext applicationContext;
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
@@ -22,5 +31,18 @@ class ApplicationTests {
 	void springSecurityFilterChainIsFilterChainProxy() {
 		Object bean = applicationContext.getBean("springSecurityFilterChain");
 		assertThat(bean).isInstanceOf(FilterChainProxy.class);
+	}
+
+	@Test
+	void defaultSecurityHeaders() throws Exception {
+		this.mockMvc.perform(get("/").secure(true))
+				.andExpect(status().isOk()) // The MessageController returns 200
+				.andExpect(header().string("X-Content-Type-Options", "nosniff"))
+				.andExpect(header().string("X-XSS-Protection", "0"))
+				.andExpect(header().string("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate"))
+				.andExpect(header().string("Pragma", "no-cache"))
+				.andExpect(header().string("Expires", "0"))
+				.andExpect(header().exists("Strict-Transport-Security")) // Will match something like "max-age=31536000 ; includeSubDomains"
+				.andExpect(header().string("X-Frame-Options", "DENY"));
 	}
 }
