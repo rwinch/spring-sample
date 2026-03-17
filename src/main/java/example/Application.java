575c8d5289dbc372574cc9b70af6f0a9b7e7ff9e
Robert Winch <362503+rwinch@users.noreply.github.com>
2026-03-17 18:14:35 -0500

    Demo Webauthn or (OTT and password)

--- src/main/java/example/Application.java
+++ src/main/java/example/Application.java
@@ -1,30 +1,55 @@
 package example;
 
-import jakarta.servlet.http.HttpServletRequest;
+import java.util.function.Predicate;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.authorization.AuthorizationDecision;
-import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authorization.AuthorityAuthorizationManager;
+import org.springframework.security.authorization.AuthorizationManagerFactories;
+import org.springframework.security.authorization.AuthorizationManagerFactories.AdditionalRequiredFactorsBuilder;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authorization.EnableMultiFactorAuthentication;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.FactorGrantedAuthority;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
 
-import java.util.function.Supplier;
+import static org.springframework.security.authorization.AuthorityAuthorizationManager.hasAuthority;
 
 @SpringBootApplication
+@EnableMultiFactorAuthentication(authorities = { FactorGrantedAuthority.OTT_AUTHORITY, FactorGrantedAuthority.PASSWORD_AUTHORITY})
 public class Application {
 
     @Bean
     SecurityFilterChain security(HttpSecurity http) throws Exception {
         return http
             .authorizeHttpRequests(requests -> requests
-                .anyRequest().access((AuthorizationManager<RequestAuthorizationContext>) (authentication, context) -> {
-                    HttpServletRequest request = context.getRequest();
-                    return null;
+                .anyRequest().authenticated()
+            )
+            .formLogin(Customizer.withDefaults())
+            .oneTimeTokenLogin(ott -> ott
+                .tokenGenerationSuccessHandler((request,response, token) -> {
+                    response.setHeader(HttpHeaders.CONTENT_TYPE, "text/plain");
+                    response.getWriter().write("Check the console");
+                    System.out.println("Use the token http://localhost:" + request.getServerPort() + "/login/ott?token=" + token.getTokenValue());
                 })
-            ).build();
+            )
+            .webAuthn(webauthn -> webauthn
+                .rpId("localhost")
+                .allowedOrigins("http://localhost:8080")
+            )
+            .build();
+    }
+
+    @Bean
+    Customizer<AdditionalRequiredFactorsBuilder<Object>> mfa() {
+        AuthorityAuthorizationManager<Object> webauthn = hasAuthority(FactorGrantedAuthority.WEBAUTHN_AUTHORITY);
+        Predicate<Authentication> notWebauthn = (a) -> !webauthn.authorize(() -> a, "").isGranted();
+        return (mfa) -> mfa
+            .when(notWebauthn);
     }
 
 	public static void main(String[] args) {
--- src/test/java/example/ApplicationTests.java
+++ src/test/java/example/ApplicationTests.java
@@ -1,12 +1,52 @@
 package example;
 
 import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.security.core.authority.FactorGrantedAuthority;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithMockUser(authorities = FactorGrantedAuthority.WEBAUTHN_AUTHORITY)
+    void whenUserHasWebauthnFactorThenAccessGranted() throws Exception {
+        this.mockMvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(authorities = FactorGrantedAuthority.PASSWORD_AUTHORITY)
+    void whenUserHasPasswordFactorThenRedirectsToLogin() throws Exception {
+        this.mockMvc.perform(get("/"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login?factor.type=ott&factor.reason=missing"));
+    }
+
+    @Test
+    @WithMockUser(authorities = FactorGrantedAuthority.OTT_AUTHORITY)
+    void whenUserHasOttFactorThenRedirectsToLogin() throws Exception {
+        this.mockMvc.perform(get("/"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login?factor.type=password&factor.reason=missing"));
+    }
+
+    @Test
+    @WithMockUser(authorities = {FactorGrantedAuthority.OTT_AUTHORITY, FactorGrantedAuthority.PASSWORD_AUTHORITY})
+    void whenUserHasPasswordAndOttFactorsThenAccessGranted() throws Exception {
+        this.mockMvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
 }
